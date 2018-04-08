27c48bf7e7316fbcb88084b8731dd405533d00a9
davdevor <drdcustomlows@gmail.com>
2018-04-08 14:51:52 -0400

    added more test runs for openmp

--- src/test/testopenmp.sh
+++ src/test/testopenmp.sh
@@ -1,13 +1,63 @@
 #!/bin/bash
 export OMP_NUM_THREADS=2
-./openmp -n 10000 -c 12 -g 821
+./openmp -n 10000 -c 12
 export OMP_NUM_THREADS=3
-./openmp -n 10000 -c 12 -g 821
+./openmp -n 10000 -c 12
 export OMP_NUM_THREADS=4
-./openmp -n 10000 -c 12 -g 821
+./openmp -n 10000 -c 12
+export OMP_NUM_THREADS=6
+./openmp -n 10000 -c 12
+export OMP_NUM_THREADS=8
+./openmp -n 10000 -c 12
 export OMP_NUM_THREADS=2
-./openmp -n 10000 -c 12 -g 821
+./openmp -n 10000 -c 12
 export OMP_NUM_THREADS=3
-./openmp -n 15000 -c 12 -g 821
+./openmp -n 12500 -c 12
 export OMP_NUM_THREADS=4
-./openmp -n 20000 -c 12 -g 821
+./openmp -n 15000 -c 12
+export OMP_NUM_THREADS=6
+./openmp -n 17500 -c 12
+export OMP_NUM_THREADS=8
+./openmp -n 20000 -c 12
+export OMP_NUM_THREADS=2
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=3
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=4
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=6
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=8
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=2
+./openmp -n 10000 -c 13
+export OMP_NUM_THREADS=3
+./openmp -n 12500 -c 13
+export OMP_NUM_THREADS=4
+./openmp -n 15000 -c 13
+export OMP_NUM_THREADS=6
+./openmp -n 17500 -c 13
+export OMP_NUM_THREADS=8
+./openmp -n 20000 -c 13
+export OMP_NUM_THREADS=2
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=3
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=4
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=6
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=8
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=2
+./openmp -n 10000 -c 14
+export OMP_NUM_THREADS=3
+./openmp -n 12500 -c 14
+export OMP_NUM_THREADS=4
+./openmp -n 15000 -c 14
+export OMP_NUM_THREADS=6
+./openmp -n 17500 -c 14
+export OMP_NUM_THREADS=8
+./openmp -n 20000 -c 14
+
+
