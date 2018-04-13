822e94530a3a6b4a83d76b8d9646d6af46ad919f
davdevor <drdcustomlows@gmail.com>
2018-04-13 08:48:02 -0400

    tested  both for two hours got output

--- src/moretesting/test.sh
+++ src/moretesting/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./serial -n 15000 -c 29 < serial.out
+./serial -n 15000 -c 29 > serial.out
 
 export OMP_NUM_THREADS=8
-./openmp -n 15000 -c 29 < openmp.out
+./openmp -n 15000 -c 29 > openmp.out
